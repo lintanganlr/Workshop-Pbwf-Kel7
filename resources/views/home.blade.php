e1acce4fef31694a15f46d7d837697a0b31de929
lintanganlr <lintangangraini3@gmail.com>
2023-09-16 22:42:12 +0700

    bikin view home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,2 @@
+@extends('layout-home')
+@section('content')
---
+++ resources/views/layout-home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GlucoSync</title>
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+<body>
+    <header class="full-width-header">
+        <!-- Isi bagian header di sini -->
+    </header>
+
+    <main class="container">
+        @yield('content')
+    </main>
+
+    <footer>
+        <!-- Isi bagian footer di sini -->
+    </footer>
+</body>
+</html>
