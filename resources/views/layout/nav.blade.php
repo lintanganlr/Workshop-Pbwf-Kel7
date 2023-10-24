b1f7d34ef6140f1d6df9b34d0e4e5fb486875e21
ameliaanindah <134347073+ameliaanindah@users.noreply.github.com>
2023-10-24 23:49:57 +0700

    menambahkan layout (nav dan footer, artikel, about, dan main

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,44 @@
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex justify-content-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope"></i> <a href="mailto:contact@example.com">contact@example.com</a>
+        <i class="bi bi-phone"></i> +1 5589 55488 55
+      </div>
+      <div class="d-none d-lg-flex social-links align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">GlucoSync.</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Pelayanan</a></li>
+          <li><a class="nav-link scrollto" href="#departments">Artikel</a></li>
+          <li><a class="nav-link scrollto" href="#testimonials">Ulasan</a></li>
+          <li class="dropdown"><a href="#"><span>Tenaga Medis</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+            <li class="dropdown"><a href="#doctors"><span>Dokter</span> <i class="bi bi-chevron-right"></i></a>
+            <li class="dropdown"><a href="#nurse"><span>Perawat</span> <i class="bi bi-chevron-right"></i></a>
+              </li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
