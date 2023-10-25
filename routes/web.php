b940a46d10aba74f2fa0dbaf39beed0015b9bc10
ameliaanindah <134347073+ameliaanindah@users.noreply.github.com>
2023-10-25 14:30:59 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home-blade');
+    return view('contact');
 });
 
 Route::get('/login', function () {
-    return view('login-blade');
+    return view('login');
 });
 
 Route::get('/regis', function () {
-    return view('register-blade');
+    return view('register');
 });
 
