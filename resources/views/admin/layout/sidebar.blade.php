dc4f2ade0ce30477a315bbf82216010929f419c7
ameliaanindah <134347073+ameliaanindah@users.noreply.github.com>
2023-11-06 09:21:35 +0700

    menjadikan navbar,sidebar ke template

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,84 +1,77 @@
-  <body>
-    <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
         <!-- Menu -->
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-          <div class="app-brand demo">
-            <a href="/admin" class="app-brand-link">GlucoSync</a>
+            <div class="app-brand demo">
+              <a href="/admin" class="app-brand-link">GlucoSync</a>
 
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-              <i class="bx bx-chevron-left bx-sm align-middle"></i>
-            </a>
-          </div>
+              <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+                <i class="bx bx-chevron-left bx-sm align-middle"></i>
+              </a>
+            </div>
 
-          <div class="menu-inner-shadow"></div>
+            <div class="menu-inner-shadow"></div>
 
-          <ul class="menu-inner py-1">
-            <!-- Dashboards -->
-            <li class="menu-item">
-              <a href="/admin" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-collection"></i>
-                <div data-i18n="Basic">Dashboard</div>
-              </a>
-            </li>
+            <ul class="menu-inner py-1">
+              <!-- Dashboards -->
+              <li class="menu-item">
+                <a href="/admin" class="menu-link">
+                  <i class="menu-icon tf-icons bx bx-collection"></i>
+                  <div data-i18n="Basic">Dashboard</div>
+                </a>
+              </li>
 
-            <li class="menu-header small text-uppercase">
-              <span class="menu-header-text">Add Ons</span>
-            </li>
-            <!-- Pages -->
-            <li class="menu-item">
-              <a href="admin.artikel" class="menu-link">
-                <i class="menu-icon bx bx-book-open"></i>
-                <div data-i18n="Basic">Artikel</div>
-              </a>
-            </li>
-            <li class="menu-item">
-              <a href="/admin" class="menu-link">
-                <i class="menu-icon bx bx bxs-comment-detail"></i>
-                <div data-i18n="Basic">Manage Review</div>
-              </a>
-            </li>
-            <li class="menu-item">
-              <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon bx bx-message-alt-add"></i>
-                <div data-i18n="Account Settings">Pelayanan</div>
-              </a>
-              <ul class="menu-sub">
-                <li class="menu-item">
-                  <a href="pages-account-settings-account.html" class="menu-link">
-                    <div data-i18n="Account">Konsultasi Dokter</div>
-                  </a>
-                </li>
-                <li class="menu-item">
-                  <a href="pages-account-settings-account.html" class="menu-link">
-                    <div data-i18n="Account">Pembersihan Luka</div>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="menu-item">
-              <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon bx bx-plus-medical"></i>
-                <div data-i18n="Account Settings">Tenaga Medis</div>
-              </a>
-              <ul class="menu-sub">
-                <li class="menu-item">
-                  <a href="pages-account-settings-account.html" class="menu-link">
-                    <div data-i18n="Account">Dokter</div>
-                  </a>
-                </li>
-                <li class="menu-item">
-                  <a href="pages-account-settings-account.html" class="menu-link">
-                    <div data-i18n="Account">Perawat</div>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </aside>
-      </div>
-    </div>
-  </body>
-</html>
+              <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">Add Ons</span>
+              </li>
+              <!-- Pages -->
+              <li class="menu-item">
+                <a href="artikel" class="menu-link">
+                  <i class="menu-icon bx bx-book-open"></i>
+                  <div data-i18n="Basic">Artikel</div>
+                </a>
+              </li>
+              <li class="menu-item">
+                <a href="/admin" class="menu-link">
+                  <i class="menu-icon bx bx bxs-comment-detail"></i>
+                  <div data-i18n="Basic">Manage Review</div>
+                </a>
+              </li>
+              <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                  <i class="menu-icon bx bx-message-alt-add"></i>
+                  <div data-i18n="Account Settings">Pelayanan</div>
+                </a>
+                <ul class="menu-sub">
+                  <li class="menu-item">
+                    <a href="pages-account-settings-account.html" class="menu-link">
+                      <div data-i18n="Account">Konsultasi Dokter</div>
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="pages-account-settings-account.html" class="menu-link">
+                      <div data-i18n="Account">Pembersihan Luka</div>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="menu-item">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                  <i class="menu-icon bx bx-plus-medical"></i>
+                  <div data-i18n="Account Settings">Tenaga Medis</div>
+                </a>
+                <ul class="menu-sub">
+                  <li class="menu-item">
+                    <a href="pages-account-settings-account.html" class="menu-link">
+                      <div data-i18n="Account">Dokter</div>
+                    </a>
+                  </li>
+                  <li class="menu-item">
+                    <a href="pages-account-settings-account.html" class="menu-link">
+                      <div data-i18n="Account">Perawat</div>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </aside>
+          <!-- / Menu -->
--- resources/views/admin/layout/template.blade.php
+++ resources/views/admin/layout/template.blade.php
@@ -10,7 +10,7 @@
     <meta charset="utf-8" />
     <meta
       name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+      content="width=device-width, initial-scale=1.0, artikel-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
 
@@ -42,12 +42,63 @@
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="../assets/js/config.js"></script>
   </head>
-  <aside class="sidebar">
-  @include('admin.layout.sidebar')
-  </aside>
-  <div class="content">
-      @yield('content')
-  </div>
+
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+
+        @include('admin.layout.sidebar')
+               <!-- Layout container -->
+               <div class="layout-page">
+                @include('admin.layout.navbar')
+        @yield('content')
+<!-- / Layout page -->
+</div>
+
+<!-- Overlay -->
+<div class="layout-overlay layout-menu-toggle"></div>
+</div>
+<!-- / Layout wrapper -->
+
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+
+<script src="../assets/vendor/libs/jquery/jquery.js"></script>
+<script src="../assets/vendor/libs/popper/popper.js"></script>
+<script src="../assets/vendor/js/bootstrap.js"></script>
+<script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="../assets/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+<script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+<!-- Main JS -->
+<script src="../assets/js/main.js"></script>
+
+<!-- Page JS -->
+<script src="../assets/js/dashboards-analytics.js"></script>
+
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
