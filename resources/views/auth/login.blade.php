d3cac8eaf3658961cdb5c835b2e106ce79e6171a
ameliaanindah <134347073+ameliaanindah@users.noreply.github.com>
2023-11-27 16:19:57 +0700

    memberi maps pada contact

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,13 @@
                                 <form method="POST" action="{{ route('login.submit') }}">
 
                                     @csrf
-                                    
+
                                     <div class="row mb-3">
                                         <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-            
+
                                         <div class="col-md-6">
                                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-            
+
                                             @error('email')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -32,10 +32,10 @@
 
                                     <div class="row mb-3">
                                         <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-                                
+
                                         <div class="col-md-6">
                                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                
+
                                             @error('password')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -71,7 +71,7 @@
                                     <div class="d-flex align-items-center justify-content-center" style="margin-top: 10px;">
                                         <p class="fs-5 mb-0 ">New to GlucoSync?</p>
                                         <a class="text-primary fw-bold ms-2"  href="register">Create an account</a>
-                                    </div> 
+                                    </div>
                                 </form>
                             </div>
                         </div>
